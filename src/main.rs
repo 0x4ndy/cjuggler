c886fe773a92fc6a78dda131c7ce9f343eb4727e
andy <andy@andy.codes>
2021-12-13 08:35:21 +0100

    Made a split between main and lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = cjuggler::run() {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
